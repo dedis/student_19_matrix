774f2c259d411dfb6e3eb6c85a2db548917303b1
Timothée Floure <timothee.floure@posteo.net>
2019-11-10 16:27:16 +0100

    Start yggdrasil node alongside proxy, although not used yet

--- cmd/yggdrasil-http-proxy/main.go
+++ cmd/yggdrasil-http-proxy/main.go
@@ -5,12 +5,19 @@ import (
 	"errors"
 	"flag"
 	"net/http"
-	"log"
 	"runtime/debug"
 	"sync"
 	"net"
+	"time"
+	"fmt"
+
+	"github.com/gologme/log"
+	"github.com/yggdrasil-network/yggdrasil-go/src/config"
+	"github.com/yggdrasil-network/yggdrasil-go/src/yggdrasil"
 
 	coap "github.com/Fnux/go-coap"
+	coapNet "github.com/Fnux/go-coap/net"
+	toyNodes "git.sr.ht/~fnux/yggdrasil-toy-nodes"
 )
 
 var (
@@ -28,6 +35,8 @@ var (
 	coapBindHost = flag.String("coap-bind-host", "0.0.0.0", "The COAP host to listen on")
 	httpBindHost = flag.String("http-bind-host", "0.0.0.0", "The HTTP host to listen on")
 	httpPort     = flag.String("http-port", "8888", "The HTTP port to listen on")
+	useconf      = flag.Bool("useconf", false, "read HJSON/JSON config from stdin")
+	useconffile  = flag.String("useconffile", "", "read HJSON/JSON config from specified file path")
 )
 
 func httpRecoverWrap(h http.Handler) http.Handler {
@@ -81,6 +90,53 @@ func main() {
 
 	// Initialize logger.
 	logger = log.New(os.Stdout, "", log.Flags())
+
+	// Initialize yggdrasil-specific environment.
+	var cfg *config.NodeConfig
+	var state *config.NodeState
+
+	// Process yggdrasil-related command-line arguments.
+	switch {
+	case *useconffile == "":
+		fmt.Println("You must provide an yggdrasil configuration.")
+		return
+	case *useconffile != "" || *useconf:
+		// Read the configuration from either stdin or from the filesystem
+		normaliseconf := false
+		cfg = toyNodes.ReadConfig(useconf, useconffile, &normaliseconf)
+	default:
+		flag.PrintDefaults()
+		return
+	}
+
+	// Initialize Yggdrasil node.
+	node := coapNet.YggdrasilNode{
+		Core: &yggdrasil.Core{},
+		Config: cfg,
+	}
+	state, err = node.Core.Start(node.Config, logger)
+	if err != nil {
+		logger.Errorln("An error occurred during Yggdrasil node startup.")
+		panic(err)
+	}
+
+	// Ignore state
+	_ = state
+
+	for len(node.Core.GetPeers()) < 2 {
+		logger.Println("Waiting for at least one active external peering...")
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	for _, peer := range node.Core.GetPeers() {
+		logger.Println("Active peering:", peer.Endpoint)
+	}
+
+	// Log some basic informations.
+	logger.Println("My node ID is:", node.Core.NodeID())
+	logger.Println("My public key is:", node.Core.EncryptionPublicKey())
+	logger.Println("My coords are:", node.Core.Coords())
+	logger.Println("Local address:", node.Core.Address().String())
 
 	// Create a wait group to keep main routine alive while HTTP and CoAP servers
 	// run in separate routines.
